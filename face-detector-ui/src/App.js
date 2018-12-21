d3bdafa24c4e9d0a458bb02354a94cb0dce19a2a
Dinesh <you@example.com>
2018-12-21 12:34:14 -0600

    UI: add Navigation, Logo

---
+++ face-detector-ui/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation.js'
+import Logo from './components/Logo/Logo.js'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <Navigation />
+         <Logo />
+      </div>
+    );
+  }
+}
+
+export default App;

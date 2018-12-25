c585d42cf3626658a9aa362113dbd6e0cde7348c
Dinesh <you@example.com>
2018-12-25 00:23:31 -0600

    api: add controllers, db

--- face-detector-api/server.js
+++ face-detector-api/server.js
@@ -1,89 +1,36 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    user : 'dinesh',
+    password : '',
+    database : 'smart-brain'
+  }
+});
 
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 
-const database = {
-	users: [
-		{
-			id: '1',
-			name: 'John',
-			email: 'john@gmail.com',
-			password: 'abc',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '2',
-			name: 'doe',
-			email: 'doe@gmail.com',
-			password: 'def',
-			entries: 0,
-			joined: new Date()
-		}
-	]
-}
+app.get('/', (req, res)=> { res.send(database.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.get('/', (req, res) => {
-	res.send(database.users);
-})
-
-app.post('/signin', (req, res) => {
-	if (req.body.email === database.users[0].email && 
-		req.body.password === database.users[0].password){
-		res.json('success');
-	} else {
-		res.status(400).json('error logging in');
-	}
-})
-
-app.post('/register', (req, res) => {
-
-	const {email, name, password} = req.body;
-	database.users.push({
-		id: '3',
-		name: name,
-		email: email,
-		password: password,
-		entries: 0,
-		joined: new Date()
-	})
-
-	res.json(database.users[database.users.length-1]);
-})
-
-app.get('/profile/:id', (req, res) => {
-	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		}
-	})
-	if (!found) {
-		res.status(400).json('not found');
-	}
-})
-
-app.put('/image', (req, res) => {
-	const {id} = req.body;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
-	})
-	if (!found) {
-		res.status(400).json('not found');
-	}
-})
-
-app.listen(3000, () => {
-	console.log('app is running on 3000 port');
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
 })
